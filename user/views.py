c5f9903d19710a0f32fc381e3377df700133c74d
Ömer Görken <omergorken35@gmail.com>
2022-05-22 22:53:25 +0300

    İkinci

--- user/views.py
+++ user/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect
-from . forms import RegisterForm,LoginForm
+from .forms import RegisterForm,LoginForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login,authenticate,logout
 from django.contrib import messages
